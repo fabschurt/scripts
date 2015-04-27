105b60d2edd1c85abec1fe9acd6483a1326187fc
Fabien Schurter <fabien.schurter@gmail.com>
2015-04-27 22:50:02 +0200

    Polish deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Ask confirmation
-echo 'All the scripts in the bin/ directory will now be copied into /usr/local/bin.'
+echo 'All the scripts in the bin/ directory will now be copied into /usr/local/bin'\
+' (this directory will be created if it does not exist).'
 read -r -p 'Are you sure you want to continue? (type [y]es or [n]o) ' response
 if [[ ! $response =~ ^(y|yes)$ ]]; then
   exit 0
 fi
 
 # Run
-cp -aXv $(dirname $0)/bin/* /usr/local/bin
+set -e
+destination='/usr/local/bin'
+if [[ ! -d $destination ]]; then
+  mkdir -pv $destination
+fi
+cp -aXv $(dirname $0)/bin/* $destination
