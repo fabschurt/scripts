cf6ea3836a607bfadf9271bd1a13549161bc5820
Fabien Schurter <fabien.schurter@gmail.com>
2015-05-28 12:12:43 +0200

    Simplify deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,7 @@
 #
 
 # Ask confirmation
-echo 'All the scripts in the bin/ directory will now be copied into /usr/local/bin'\
-' (this directory will be created if it does not exist).'
+echo 'All the scripts in the bin/ directory will now be copied into /usr/local/bin.'
 read -r -p 'Are you sure you want to continue? (type [y]es or [n]o) ' response
 if [[ ! $response =~ ^(y|yes)$ ]]; then
   exit 0
@@ -22,8 +21,4 @@ fi
 
 # Run
 set -e
-destination='/usr/local/bin'
-if [[ ! -d $destination ]]; then
-  mkdir -pv $destination
-fi
-cp -aXv $(dirname $0)/bin/* $destination
+cp -aXv $(dirname $0)/bin/* /usr/local/bin
